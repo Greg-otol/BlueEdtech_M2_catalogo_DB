da84d396702b5af894669d60996df86a634434b9
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-06-05 17:43:38 -0300

    ssl

--- src/database/db.js
+++ src/database/db.js
@@ -9,6 +9,12 @@ const connection = new Sequelize(
     // host: process.env.DB_HOST,
     // port: 5432,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
