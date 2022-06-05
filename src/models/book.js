3e7096b407f413241ade4d5bc3a15fb6b6e10997
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-06-05 17:48:12 -0300

    update

--- src/models/book.js
+++ src/models/book.js
@@ -1,35 +1,44 @@
-const Sequelize = require('sequelize');
-const connection = require('../database/db');
+const Sequelize = require("sequelize");
+const connection = require("../database/db");
 
-const Book = connection.define("books", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Book = connection.define(
+  "books",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    synopsis: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  image: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  synopsis: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  author: {
-    type: Sequelize.STRING,
-    allowNull: false,
+  {
+    freezeTableName: true,
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
   }
-},
-{
-  freezeTableName: true,
-  timestamps: false, 
-  createdAt: false,
-  updatedAt: false,
-});
+);
+
+const initTable = async () => {
+  await Book.sync();
+};
+
+initTable();
 
 module.exports = Book;
