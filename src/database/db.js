2d952e9ccf8d8be3a4df40116bc01f4774cc71f4
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-06-05 17:39:44 -0300

    Postgres

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DATABASE_URL,
+  // process.env.DB_BASE,
+  // process.env.DB_USER,
+  // process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
-    port: 5432,
+    // host: process.env.DB_HOST,
+    // port: 5432,
     dialect: "postgres",
   }
 );
