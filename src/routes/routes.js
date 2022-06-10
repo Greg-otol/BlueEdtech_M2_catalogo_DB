81da363b576c0b8a41bd0b919661509453ea6d5f
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-06-10 19:01:24 -0300

    Update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,11 @@ const routes = require("express").Router();
 const bookController = require("../controllers/bookController");
 
 routes.get("/", bookController.getAll);
-
 routes.get("/register", bookController.register);
-
 routes.get("/details/:id", bookController.details);
-
 routes.post("/create", bookController.create);
-
 routes.get("/update/:id", bookController.getUpdate);
 routes.post("/update/:id", bookController.postUpdate)
-
 routes.get("/delete/:id", bookController.remove);
 
 module.exports = routes;
