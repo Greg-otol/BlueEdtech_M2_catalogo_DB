802028eb39c71f222b139e5c77b59e43f376af77
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-06-12 11:00:06 -0300

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,32 @@
-let imported = document.createElement('script');
-document.head.appendChild(imported);
+// let imported = document.createElement('script');
+// document.head.appendChild(imported);
 
-let show = true;
+// let show = true;
 
-const menuSection = document.querySelector(".menu-section");
-const menuToggle = menuSection.querySelector(".menu-toggle");
+// const menuSection = document.querySelector(".menu-section");
+// const menuToggle = menuSection.querySelector(".menu-toggle");
 
-menuToggle.addEventListener("click", () => {
-  document.body.style.overflow = show ? "hidden" : "initial";
+// menuToggle.addEventListener("click", () => {
+//   document.body.style.overflow = show ? "hidden" : "initial";
 
-  menuSection.classList.toggle("on", show);
-  show = !show;
-});
+//   menuSection.classList.toggle("on", show);
+//   show = !show;
+// });
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
